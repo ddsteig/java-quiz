801bc898405eb5520b88e925c8a4bc8b28408883
Dani <tekhexrax@yahoo.com>
2020-07-05 22:24:14 -0400

    Upadted index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Start button with a click event
+    // Run quiz function
+
+// Timed Quiz... function
+    // Timer counts down
+        //Click event for answer buttons
+            // If question is answered wrong, return wrong, deduct time and move to next question
+            // Else return correct & move to next question
+
+// Quiz Questions & Answers.... possible object / array
+    // Decide how many questions
+
+// Score... stylized box with input
+    // Input for name + display score... prevent default possibly used here
+    //High score display
+
